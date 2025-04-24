b37653ae859d1d27e7123ff56c37395308007db2
Sihun Kim <skim585@asu.edu>
2025-04-24 15:53:18 -0700

    feat(apps/logging/): init logging service inside monorepo. initiate mongoose
    init logging service, init mongoose for connection, init docker compose for mongodb

--- apps/logging/src/logging.service.ts
+++ apps/logging/src/logging.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class LoggingServiceService {
+export class LoggingService {
   getHello(): string {
-    return '[LOGGING SERVICE]: Hello World!';
+    return 'Hello World!';
   }
 }
