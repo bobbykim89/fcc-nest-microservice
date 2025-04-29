f241ca717669969d9f434806cacb6cd2da86c488
Sihun Kim <skim585@asu.edu>
2025-04-29 13:42:34 -0700

    feat(apps/logging/): init mongoose schema, add dto
    init mongoose schema for rider coordinates, and add dto for post req

--- apps/logging/src/rider-coordinates/rider-coordinates.controller.ts
+++ apps/logging/src/rider-coordinates/rider-coordinates.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCoordinatesDto } from './dto';
 
 @Controller('rider-coordinates')
 export class RiderCoordinatesController {
   @Get()
   getRiderCoordinates() {
     return 'Hello I am from rider coordinates';
+  }
+
+  @Post()
+  saveRiderCoordinates(@Body() createCoordinatesDto: CreateCoordinatesDto) {
+    return createCoordinatesDto;
   }
 }
